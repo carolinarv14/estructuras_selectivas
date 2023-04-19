63d1973ab80120d58d6a453be3542f5dd876650c
Carolina <carolinarinconv@iesantaelena.edu.co>
2023-04-19 16:16:22 -0500

    cambios

---
+++ script/Ejercicio0.js
@@ -0,0 +1,17 @@
+let cantidad, descuento, monto, totalPagar
+
+//  Leer o asignar informacion 
+cantidad =  Number(prompt ('ingrese cantidad'))
+
+// proceso
+monto = cantidad * 2000000;
+
+
+
+if (cantidad<10) {
+    alert('no tiene descuento')
+} else if (cantidad<20 && cantidad<20) {
+    descuento=monto*0.1
+}else if(cantidad>=20 && cantidad<30){
+
+}
